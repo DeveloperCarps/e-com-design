2248cf738d5ac7276ae1a5fdea062cf278963947
Deepbleu Carpeso <dev.carpeso@gmail.com>
2024-11-19 13:00:14 +0800

    Update 2!

--- script.js
+++ script.js
@@ -1,224 +1,89 @@
-const components = [
-    {
-        name: 'hero-slide',
-        class: class HeroSlide extends HTMLElement {
-            static observedAttributes = ['title', 'subtitle', 'image'];
-            constructor() {
-                super();
-                this.attachShadow({ mode: 'open' });
-            }
-            connectedCallback() {
-                this.shadowRoot.innerHTML = `
-                    <style>
-                        :host {
-                            width: 100%;
-                            height: 100%;
-                            padding: 40px;
-                            background: rgb(0,0,0);
-                            background: linear-gradient(0deg, rgba(0,0,0,0.6572829815519958) 30%, rgba(255,255,255,0) 94%); 
+function add_slide(image_url, title, subtitle, id) {
+    const containerDiv = document.createElement('div');
+    containerDiv.classList.add('content');
 
-                            display: flex;
-                            flex-flow: column nowrap;
-                            justify-content: end;
-                            align-items: start;
-                            background-image: url(${this.image});
-                            background-position: center;
-                            background-repeat: no-repeat;
-                            background-size: cover;
-                        }
-                        .slide {
-                            width: 100%;
-                            flex-shrink: 0;
-                            height: 100%;
-                            scroll-snap-align: start;
-                            background-position: center;
-                            background-repeat: no-repeat;
-                            background-size: cover;
-                        }
-                        .slide-header {
-                            color: white;
-                            font-size: 4em;
-                            font-weight: 800;
-                            margin: 0;
-                        }
-                        .slide-span {
-                            color: white;
-                            font-size: 1.2em;
-                            font-weight: 500;
-                            margin: 0;
-                        }
-                        .hero-button {
-                            margin-top: 20px;
-                            padding: 15px 30px;
-                            width: fit-content;
+    let div = document.createElement('div');
+    let header = document.createElement("h1");
+    let span = document.createElement("span");
+    let anchor = document.createElement("a");
 
-                            display: block;
-                            background-color: #ffda59;
-                            border: none;
-                            border-radius: 5px;
-                            cursor: pointer;
-                            color: #091423;
-                            font-size: 1em;
-                            font-weight: 500;
-                            outline: none;
-                            text-decoration: none;
-                        }
-                    </style>
-                    <div class="content">
-                        <h1 class="slide-header">${this.title}</h1>
-                        <span class="slide-span">${this.subtitle}</span>
-                        <a id="${this.id}" class="hero-button">Shop now</a>
-                    </div>
-                `;
-            }
-            attributeChangedCallback(name, oldValue, newValue) {
-                if (name === 'title') {
-                    this.title = newValue;
-                }
-                if (name === 'subtitle') {
-                    this.subtitle = newValue;
-                }
-                if (name === 'image') {
-                    this.image = newValue;
-                }
-            }
-        }
-    },
-    {
-        name: 'product-card',
-        class: class ProductCard extends HTMLElement {
-            static observedAttributes = ['title', 'desc', 'image', 'id', 'price'];
-            constructor() {
-                super();
-                this.attachShadow({ mode: 'open' });
-            }
-            connectedCallback() {
-                this.shadowRoot.innerHTML = `
-                    <style>
-                        :host {
-                            margin: 0 15px;
-                        }
-                        .card {
-                            width: 300px;
-                            height: 425px;
-                            background-color: white;
-                            border-radius: 5px;
-                            display: flex;
-                            flex-direction: column;
-                            align-items: start;
-                            justify-content: start;
-                        }
-                        a {
-                            box-sizing: border-box;
-                            background-color: #091423;
-                            color: white;
-                            padding: 10px;
-                            border-radius: 5px;
-                            text-decoration: none;
-                            margin-top: auto;
-                            font-size: 1rem;
-                            width: 100%;
-                            text-align: center;
-                            cursor: pointer;
-                        }
-                        .image {
-                            display: block;
-                            background-image: url(${this.image});
-                            background-size: cover;
-                            background-position: center;
-                            background-repeat: no-repeat;
-                            width: 100%;
-                            height: auto;
-                            aspect-ratio: 1/1;
-                            border-radius: 5px;
-                        }
-                        h3 {
-                            font-size: 1.5rem;
-                            margin: 10px 0 0 0;
-                        }
-                        .pricing {
-                            margin-top: auto;
-                            display: grid;
-                            width: 100%;
-                            grid-template-columns: 1fr 90px 40px;
-                            gap: 5px;
-                            height: 40px;
+    div.classList.add('slide');
+    header.classList.add('slide-header');
+    span.classList.add('slide-span');
+    anchor.classList.add('hero-button');
 
-                            & a {
-                                background: none;
-                                border: 1px solid #e0e0e0;
-                                color: black;
-                                padding: 5px 0;
-                                border-radius: 5px;
-                                text-decoration: none;
-                                font-size: 1rem;
-                                text-align: center;
-                                cursor: pointer;
-                                height: 100%;
+    div.style.backgroundImage = `url(${image_url})`;
+    header.textContent = title;
+    span.textContent = subtitle;
+    anchor.textContent = 'Shop now';
+    anchor.id = id;
 
-                                display: flex;
-                                align-items: center;
-                                justify-content: center;
+    div.appendChild(header);
+    div.appendChild(span);
+    div.appendChild(anchor);
 
-                                & img {
-                                    width: 20px;
-                                    height: 20px;
-                                    box-sizing: border-box;
-                                }
-                            }
+    containerDiv.appendChild(div);
 
-                            & p {
-                                margin: 0;
-                                font-size: 1.75rem;
-                                display: flex;
-                                align-items: center;
-                                justify-content: start;
-                            }
-                        }
-                        span {
-                            font-size: 1rem;
-                            margin: 0 0 10px 0;
-                        }
-                    </style>
-                    <div class="card">
-                        <div class="image"></div>
-                        <h3>${this.title}</h3>
-                        <span>${this.desc}</span>
-                        <div class="pricing">
-                            <p>${this.price}</p>
-                            <a id=${this.id}>Buy now</a>
-                            <a id=${this.id}>
-                                <img src="assets/plus.svg"></img>
-                            </a>
-                        </div>
-                    </div>
-                `;
-            }
-            attributeChangedCallback(name, oldValue, newValue) {
-                if (name === 'title') {
-                    this.title = newValue;
-                }
-                if (name === 'desc') {
-                    this.desc = newValue;
-                }
-                if (name === 'image') {
-                    this.image = newValue;
-                }
-                if (name === 'id') {
-                    this.id = newValue;
-                }
-                if (name === 'price') {
-                    this.price = newValue;
-                }
-            }
-
-        }
-    }
-];
-components.forEach(component => {
-    customElements.define(component.name, component.class);
-});
-
-function slides() {
-
+    return containerDiv;
 }
+
+function add_product_card(image_url, title, desc, price, id) {
+    const card = document.createElement("div");
+    const image = document.createElement("div");
+
+    image.style.backgroundImage = `url(${image_url})`;
+    image.classList.add('image');
+    card.classList.add('card');
+
+    const h3 = document.createElement("h3");
+    h3.textContent = title;
+
+    const span = document.createElement("span");
+    span.textContent = desc;
+
+    const pricing = document.createElement("div");
+    pricing.classList.add('pricing');
+
+    const p = document.createElement("p");
+    p.textContent = price;
+
+    const buy_now = document.createElement("a");
+    buy_now.textContent = 'Buy now';
+    buy_now.id = id;
+
+    const plus = document.createElement("a");
+    plus.id = id;
+    const img = document.createElement("img");
+    img.src = 'assets/plus.svg';
+    plus.appendChild(img);
+
+    pricing.appendChild(p);
+    pricing.appendChild(buy_now);
+    pricing.appendChild(plus);
+
+    card.appendChild(image);
+    card.appendChild(h3);
+    card.appendChild(span);
+    card.appendChild(pricing);
+
+    return card;
+}
+
+const hero = document.getElementById('hero');
+hero.appendChild(add_slide('assets/one.jpg', 'New Collection', 'Summer 2021', 'hero1'));
+
+const products = document.getElementById('new_arrivals');
+products.appendChild(add_product_card('assets/1.jpeg', 'Product 1', 'Description', 'PHP 13,500', 'product1'));
+
+products.appendChild(add_product_card('assets/2.jpeg', 'Product 2', 'Description', 'PHP 7,250', 'product2'));
+
+products.appendChild(add_product_card('assets/3.jpeg', 'Product 3', 'Description', 'PHP 9,900', 'product3'));
+
+const popular = document.getElementById('popular');
+
+popular.appendChild(add_product_card('assets/4.jpeg', 'Product 4', 'Description', 'PHP 5,500', 'product4'));
+
+popular.appendChild(add_product_card('assets/5.jpeg', 'Product 5', 'Description', 'PHP 8,500', 'product5'));
+
+popular.appendChild(add_product_card('assets/6.jpeg', 'Product 6', 'Description', 'PHP 6,500', 'product6'));
+
